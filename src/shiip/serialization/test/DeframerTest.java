17910a12446b07e9cbe25ab4aa2462c74d325843
Andrew Walker <drewwalker76@gmail.com>
2019-09-03 21:21:13 -0500

    Groups tests

--- src/shiip/serialization/test/DeframerTest.java
+++ src/shiip/serialization/test/DeframerTest.java
@@ -9,6 +9,7 @@
 package shiip.serialization.test;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import shiip.serialization.Deframer;
 import shiip.serialization.SerializationConstants;
@@ -22,88 +23,98 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class DeframerTest {
 
-    @Test
-    @DisplayName("constructor - Null InputStream")
-    public void testConstructorNull(){
-        assertThrows(NullPointerException.class, () -> new Deframer(null));
+    @Nested
+    @DisplayName("constructor")
+    public class Constructor{
+
+        @Test
+        @DisplayName("Null InputStream")
+        public void testConstructorNull(){
+            assertThrows(NullPointerException.class, () -> new Deframer(null));
+        }
     }
 
-    @Test
-    @DisplayName("getFrame - No payload")
-    public void testGetFrameNoPayload() throws IOException {
-        byte[] message = new byte[]{0, 0, 0, 5, 5, 5, 5, 5, 5};
-        ByteArrayInputStream in = new ByteArrayInputStream(message);
-        Deframer deframer = new Deframer(in);
+    @Nested
+    @DisplayName("getFrame")
+    public class GetFrame{
 
-        byte[] res = deframer.getFrame();
-        assertArrayEquals(new byte[] {5, 5, 5, 5, 5, 5}, res);
-    }
+        @Test
+        @DisplayName("No payload")
+        public void testGetFrameNoPayload() throws IOException {
+            byte[] message = new byte[]{0, 0, 0, 5, 5, 5, 5, 5, 5};
+            ByteArrayInputStream in = new ByteArrayInputStream(message);
+            Deframer deframer = new Deframer(in);
 
-    @Test
-    @DisplayName("getFrame - Valid payload")
-    public void testGetFrameValidPayload() throws IOException {
-        byte[] message = new byte[]{0, 0, 1, 5, 5, 5, 5, 5, 5, 2};
-        ByteArrayInputStream in = new ByteArrayInputStream(message);
-        Deframer deframer = new Deframer(in);
+            byte[] res = deframer.getFrame();
+            assertArrayEquals(new byte[] {5, 5, 5, 5, 5, 5}, res);
+        }
 
-        byte[] res = deframer.getFrame();
-        assertArrayEquals(new byte[] {5, 5, 5, 5, 5, 5, 2}, res);
-    }
+        @Test
+        @DisplayName("Valid payload")
+        public void testGetFrameValidPayload() throws IOException {
+            byte[] message = new byte[]{0, 0, 1, 5, 5, 5, 5, 5, 5, 2};
+            ByteArrayInputStream in = new ByteArrayInputStream(message);
+            Deframer deframer = new Deframer(in);
 
-    @Test
-    @DisplayName("getFrame - Maximum length payload")
-    public void testGetFrameMaximumPayload() throws IOException {
-        byte[] message = new byte[SerializationConstants.HEADER_BYTES + SerializationConstants.MAXIMUM_PAYLOAD_LENGTH_BYTES + 3];
-        message[1] = 0x40;
-        ByteArrayInputStream in = new ByteArrayInputStream(message);
-        Deframer deframer = new Deframer(in);
+            byte[] res = deframer.getFrame();
+            assertArrayEquals(new byte[] {5, 5, 5, 5, 5, 5, 2}, res);
+        }
 
-        byte[] solution = new byte[SerializationConstants.HEADER_BYTES + SerializationConstants.MAXIMUM_PAYLOAD_LENGTH_BYTES];
+        @Test
+        @DisplayName("Maximum length payload")
+        public void testGetFrameMaximumPayload() throws IOException {
+            byte[] message = new byte[SerializationConstants.HEADER_BYTES + SerializationConstants.MAXIMUM_PAYLOAD_LENGTH_BYTES + 3];
+            message[1] = 0x40;
+            ByteArrayInputStream in = new ByteArrayInputStream(message);
+            Deframer deframer = new Deframer(in);
 
-        assertArrayEquals(solution, deframer.getFrame());
-    }
+            byte[] solution = new byte[SerializationConstants.HEADER_BYTES + SerializationConstants.MAXIMUM_PAYLOAD_LENGTH_BYTES];
 
-    @Test
-    @DisplayName("getFrame - Empty payload")
-    public void testPutFrameNullPayload(){
-        byte[] message = new byte[]{};
-        ByteArrayInputStream in = new ByteArrayInputStream(message);
-        Deframer deframer = new Deframer(in);
+            assertArrayEquals(solution, deframer.getFrame());
+        }
 
-        assertThrows(EOFException.class, deframer::getFrame);
-    }
+        @Test
+        @DisplayName("Empty payload")
+        public void testPutFrameNullPayload(){
+            byte[] message = new byte[]{};
+            ByteArrayInputStream in = new ByteArrayInputStream(message);
+            Deframer deframer = new Deframer(in);
 
-    @Test
-    @DisplayName("getFrame - Oversized payload")
-    public void testPutFrameOversizedPayload(){
-        byte[] message = new byte[SerializationConstants.HEADER_BYTES + SerializationConstants.MAXIMUM_PAYLOAD_LENGTH_BYTES + 4];
-        message[1] = 0x40;
-        message[2] = 0x01;
-        ByteArrayInputStream in = new ByteArrayInputStream(message);
-        Deframer deframer = new Deframer(in);
+            assertThrows(EOFException.class, deframer::getFrame);
+        }
 
-        assertThrows(IOException.class, deframer::getFrame);
-    }
+        @Test
+        @DisplayName("Oversized payload")
+        public void testPutFrameOversizedPayload(){
+            byte[] message = new byte[SerializationConstants.HEADER_BYTES + SerializationConstants.MAXIMUM_PAYLOAD_LENGTH_BYTES + 4];
+            message[1] = 0x40;
+            message[2] = 0x01;
+            ByteArrayInputStream in = new ByteArrayInputStream(message);
+            Deframer deframer = new Deframer(in);
 
-    @Test
-    @DisplayName("getFrame - Extra data payload")
-    public void testPutFrameExtraDataPayload(){
-        byte[] message = new byte[SerializationConstants.HEADER_BYTES + SerializationConstants.MAXIMUM_PAYLOAD_LENGTH_BYTES + 4];
-        message[1] = 0x40;
-        ByteArrayInputStream in = new ByteArrayInputStream(message);
-        Deframer deframer = new Deframer(in);
+            assertThrows(IOException.class, deframer::getFrame);
+        }
 
-        assertThrows(IOException.class, deframer::getFrame);
-    }
+        @Test
+        @DisplayName("Extra data payload")
+        public void testPutFrameExtraDataPayload(){
+            byte[] message = new byte[SerializationConstants.HEADER_BYTES + SerializationConstants.MAXIMUM_PAYLOAD_LENGTH_BYTES + 4];
+            message[1] = 0x40;
+            ByteArrayInputStream in = new ByteArrayInputStream(message);
+            Deframer deframer = new Deframer(in);
 
-    @Test
-    @DisplayName("getFrame - Missing headers")
-    public void testPutFrameMissingHeaders(){
-        byte[] message = new byte[]{0, 0, 1};
-        ByteArrayInputStream in = new ByteArrayInputStream(message);
-        Deframer deframer = new Deframer(in);
+            assertThrows(IOException.class, deframer::getFrame);
+        }
 
-        assertThrows(EOFException.class, deframer::getFrame);
+        @Test
+        @DisplayName("Missing headers")
+        public void testPutFrameMissingHeaders(){
+            byte[] message = new byte[]{0, 0, 1};
+            ByteArrayInputStream in = new ByteArrayInputStream(message);
+            Deframer deframer = new Deframer(in);
+
+            assertThrows(EOFException.class, deframer::getFrame);
+        }
     }
 
 }
--- src/shiip/serialization/test/FramerTest.java
+++ src/shiip/serialization/test/FramerTest.java
@@ -9,6 +9,7 @@
 package shiip.serialization.test;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import shiip.serialization.Framer;
 import shiip.serialization.SerializationConstants;
@@ -20,68 +21,77 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class FramerTest {
 
-    @Test
-    @DisplayName("constructor - Null outputstream")
-    public void testConstructorNull(){
-        assertThrows(NullPointerException.class, () -> new Framer(null));
+    @Nested
+    @DisplayName("constructor")
+    public class Constructor{
+
+        @Test
+        @DisplayName("Null OutputStream")
+        public void testConstructorNull(){
+            assertThrows(NullPointerException.class, () -> new Framer(null));
+        }
     }
 
-    @Test
-    @DisplayName("putFrame - No payload")
-    public void testPutFrameNoPayload(){
-        byte[] message = new byte[]{5, 5, 5, 5, 5, 5};
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        Framer framer = new Framer(out);
+    @Nested
+    @DisplayName("putFrame")
+    public class PutFrame{
+        @Test
+        @DisplayName("No payload")
+        public void testPutFrameNoPayload(){
+            byte[] message = new byte[]{5, 5, 5, 5, 5, 5};
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            Framer framer = new Framer(out);
 
-        assertDoesNotThrow(() -> framer.putFrame(message));
-        assertArrayEquals(new byte[] {0, 0, 0, 5, 5, 5, 5, 5, 5}, out.toByteArray());
-    }
+            assertDoesNotThrow(() -> framer.putFrame(message));
+            assertArrayEquals(new byte[] {0, 0, 0, 5, 5, 5, 5, 5, 5}, out.toByteArray());
+        }
 
-    @Test
-    @DisplayName("putFrame - Valid payload")
-    public void testPutFrameValidPayload(){
-        byte[] message = new byte[]{5, 5, 5, 5, 5, 5, 2};
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        Framer framer = new Framer(out);
+        @Test
+        @DisplayName("Valid payload")
+        public void testPutFrameValidPayload(){
+            byte[] message = new byte[]{5, 5, 5, 5, 5, 5, 2};
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            Framer framer = new Framer(out);
 
-        assertDoesNotThrow(() -> framer.putFrame(message));
-        assertArrayEquals(new byte[] {0, 0, 1, 5, 5, 5, 5, 5, 5, 2}, out.toByteArray());
-    }
+            assertDoesNotThrow(() -> framer.putFrame(message));
+            assertArrayEquals(new byte[] {0, 0, 1, 5, 5, 5, 5, 5, 5, 2}, out.toByteArray());
+        }
 
-    @Test
-    @DisplayName("putFrame - Maximum length payload")
-    public void testPutFrameMaxPayload(){
-        byte[] message = new byte[SerializationConstants.HEADER_BYTES + SerializationConstants.MAXIMUM_PAYLOAD_LENGTH_BYTES];
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        Framer framer = new Framer(out);
+        @Test
+        @DisplayName("Maximum length payload")
+        public void testPutFrameMaxPayload(){
+            byte[] message = new byte[SerializationConstants.HEADER_BYTES + SerializationConstants.MAXIMUM_PAYLOAD_LENGTH_BYTES];
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            Framer framer = new Framer(out);
 
-        byte[] solution = new byte[SerializationConstants.HEADER_BYTES + SerializationConstants.MAXIMUM_PAYLOAD_LENGTH_BYTES + 3];
-        solution[1] = 0x40;
+            byte[] solution = new byte[SerializationConstants.HEADER_BYTES + SerializationConstants.MAXIMUM_PAYLOAD_LENGTH_BYTES + 3];
+            solution[1] = 0x40;
 
-        assertDoesNotThrow(() -> framer.putFrame(message));
-        assertArrayEquals(solution, out.toByteArray());
-    }
+            assertDoesNotThrow(() -> framer.putFrame(message));
+            assertArrayEquals(solution, out.toByteArray());
+        }
 
-    @Test
-    @DisplayName("putFrame - Null payload")
-    public void testPutFrameNullPayload(){
-        Framer framer = new Framer(System.out);
-        assertThrows(NullPointerException.class, () -> framer.putFrame(null));
-    }
+        @Test
+        @DisplayName("Null payload")
+        public void testPutFrameNullPayload(){
+            Framer framer = new Framer(System.out);
+            assertThrows(NullPointerException.class, () -> framer.putFrame(null));
+        }
 
-    @Test
-    @DisplayName("putFrame - Oversized payload")
-    public void testPutFrameOversizedPayload(){
-        byte[] message = new byte[SerializationConstants.HEADER_BYTES + SerializationConstants.MAXIMUM_PAYLOAD_LENGTH_BYTES + 1];
-        Framer framer = new Framer(System.out);
-        assertThrows(IOException.class, () -> framer.putFrame(message));
-    }
+        @Test
+        @DisplayName("Oversized payload")
+        public void testPutFrameOversizedPayload(){
+            byte[] message = new byte[SerializationConstants.HEADER_BYTES + SerializationConstants.MAXIMUM_PAYLOAD_LENGTH_BYTES + 1];
+            Framer framer = new Framer(System.out);
+            assertThrows(IOException.class, () -> framer.putFrame(message));
+        }
 
-    @Test
-    @DisplayName("putFrame - Missing headers")
-    public void testPutFrameMissingHeaders(){
-        byte[] message = new byte[SerializationConstants.HEADER_BYTES - 5];
-        Framer framer = new Framer(System.out);
-        assertThrows(IOException.class, () -> framer.putFrame(message));
+        @Test
+        @DisplayName("Missing headers")
+        public void testPutFrameMissingHeaders(){
+            byte[] message = new byte[SerializationConstants.HEADER_BYTES - 5];
+            Framer framer = new Framer(System.out);
+            assertThrows(IOException.class, () -> framer.putFrame(message));
+        }
     }
 }
